f7955aa678b90c4dc8eace528c0133628a33b45e
Sylvester Mobley <sylvestermobley@Sylvesters-MBP.home>
2018-09-03 19:46:07 -0400

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,85 @@
+'use strict';
+
+$(function() {
+
+  page.config({
+
+    /*
+    |--------------------------------------------------------------------------
+    | Google API Key
+    |--------------------------------------------------------------------------
+    |
+    | Specify Google API key for Google Maps
+    |
+    | https://developers.google.com/maps/documentation/javascript/get-api-key
+    |
+    */
+
+    googleApiKey: '',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Google Analytics Tracking
+    |--------------------------------------------------------------------------
+    |
+    | Specify Google Analytics Tracking ID
+    | Key would be a value like: UA-12345678-9
+    |
+    */
+
+    googleAnalyticsId: '',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Google reCAPTCHA
+    |--------------------------------------------------------------------------
+    |
+    | Specify Google reCAPTCHA `Site key`
+    |
+    */
+
+    reCaptchaSiteKey:  '',
+
+    // See available languages: https://developers.google.com/recaptcha/docs/language
+    reCaptchaLanguage: '',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Disable AOS on mobile
+    |--------------------------------------------------------------------------
+    |
+    | If true, the Animate On Scroll animations won't run on mobile devices.
+    |
+    */
+
+    disableAOSonMobile: true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Smooth Scroll
+    |--------------------------------------------------------------------------
+    |
+    | If true, the browser's scrollbar moves smoothly on scroll.
+    |
+    */
+
+    smoothScroll: true,
+
+  });
+
+
+
+
+
+  /*
+  |--------------------------------------------------------------------------
+  | Custom Javascript code
+  |--------------------------------------------------------------------------
+  |
+  |
+  */
+
+
+
+
+});
